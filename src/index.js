f307c052693edf775277bd610d83bc914e770878
victorze <victorzevallos@protonmail.com>
2019-03-07 13:01:48 -0500

    validaciónd de datos

--- src/index.js
+++ src/index.js
@@ -1,51 +1,15 @@
 const express = require('express');
-const { routeHelper, sleep, addUserToDB } = require('./route');
+
+const validations = require('./validations');
 
 const app = express();
 
-async function getUser() {
-    return {
-        id: 123,
-        name: 'victor',
-    }
-}
+app.use(express.json());
 
-// global: todas las rutas ejecutan este middleware
-app.use((req, res, next) => {
-    if (req.ip == '182.15.25.48') {
-        next(new Error('error!!!'));
-    }
-    next();
-});
-
-const foo = async (req, res, next) => {
-    const user = await getUser();
-
-    req.locals = {
-        user,
-    };
-
-    next();
-};
-
-// el middleware que verifica la ip se ejecuta antes
-app.get('/test', async (req, res) => {
-    const user = req.locals.user;
-
+app.post('/users', validations.validate(validations.createUsersValidation),
+        (req, res) => {
     res.json({
         status: 'ok',
-        user,
-    });
-});
-
-// el middleware que verifica la ip se ejecuta antes
-// leugo se ejecuta el middleware foo
-app.get('/test-2', foo, async (req, res) => {
-    const user = req.locals.user;
-
-    res.json({
-        status: 'ok',
-        user,
     });
 });
 
